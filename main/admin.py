2cf65681639e20e8a2a5d6e37973cfbb5f9a4aa5
Penistrong <770560618@qq.com>
2020-09-23 15:06:01 +0800

    Pure Edition

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import LabeledImage, ClassifiedType
+from users.models import UserProfile
+
+admin.site.register(LabeledImage)
+admin.site.register(ClassifiedType)
+admin.site.register(UserProfile)
